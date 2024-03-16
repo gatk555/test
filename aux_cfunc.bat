dc1f855b933e23b4482ccbf121c977df415ae759
ga <ga@oldell.fish>
2024-03-16 09:17:07 +0000

    Redirect script output.

--- aux_cfunc.bat
+++ aux_cfunc.bat
@@ -3,7 +3,7 @@ rem    .\aux-cfunc.bat analog
 
 set sub=%1
 
-echo Script in %sub%
+echo Script in %sub% > log.txt
 
 set CMPP_IDIR=../../src/xspice/icm/%sub%
 set CMPP_ODIR=icm/%sub%
@@ -11,15 +11,15 @@ if not exist icm\%sub% mkdir icm\%sub%
 .\bin\cmpp -lst
 
 for /F %%n in (..\..\src\xspice\icm\%sub%\modpath.lst) do (
-  echo Starting %%n
+  echo Starting %%n >> log.txt
   set CMPP_IDIR=../../src/xspice/icm/%sub%/%%n
   set CMPP_ODIR=icm/%sub%/%%n
   if not exist icm\%sub%\%%n mkdir icm\%sub%\%%n
   .\bin\cmpp -ifs
-  echo Done ifs
+  echo Done ifs >> log.txt
   .\bin\cmpp -mod
   pushd icm\%sub%\%%n
-  echo In subdir icm\%sub%\%%n
+  echo In subdir icm\%sub%\%%n >> log.txt
   if exist %%n-cfunc.c del %%n-cfunc.c
   if exist %%n-ifspec.c del %%n-ifspec.c
   rename cfunc.c %%n-cfunc.c
