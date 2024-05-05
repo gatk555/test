74bd1c21cbc248205ce248d90ddaab5336e338ea
ga <ga@oldell.fish>
2024-05-05 18:29:43 +0100

    Include default path.

--- btest/a/b/aux-ivl.bat
+++ btest/a/b/aux-ivl.bat
@@ -5,11 +5,9 @@ cd verilog
 set src=..\..\..\src\xspice\verilog
 set inc=..\..\..\src\include
 CL.EXE /O2 /LD /EHsc /Feivlng.DLL /I%src% /I%inc% %src%\icarus_shim.c /link /EXPORT:Cosim_setup /EXPORT:Get_ng_vvp
-dir
 
 rem Make a dummy libvvp.obj, needed for shim.vpi (to be renamed ivlng.vpi).
 
 lib.exe /def:%src%\libvvp.def /machine:X64
-dir
-CL.EXE /O2 /LD /EHsc /Feshim.vpi /I. /I%inc% %src%\vpi.c libvvp.lib ivlng.lib /link /DLL /EXPORT:vlog_startup_routines
+CL.EXE /O2 /LD /EHsc /Feivlng.DLL /I%src% /I%inc% /DNGSPICELIBDIR=C:\ngspice64\lib\ngspice %src%\icarus_shim.c /link /EXPORT:Cosim_setup /EXPORT:Get_ng_vvp
 dir
