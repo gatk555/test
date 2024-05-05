e27ac01b2df936620d47e42e9e1b0c60c783afc2
ga <ga@oldell.fish>
2024-05-05 15:54:53 +0100

    Build IVLNG components with script.

---
+++ btest/a/b/aux-ivl.bat
@@ -0,0 +1,15 @@
+rem Make support components for Icarus Verilog co-simulation:
+rem ivlng.dll and ivlng.vpi.
+
+cd verilog
+set src=..\..\..\src\xspice\verilog
+set inc=..\..\..\src\include
+CL.EXE /O2 /LD /EHsc /Feivlng.DLL /I%src% /I%inc% %src%\icarus_shim.c /link /EXPORT:Cosim_setup /EXPORT:Get_ng_vvp
+dir
+
+rem Make a dummy libvvp.obj, needed for shim.vpi (to be renamed ivlng.vpi).
+
+lib.exe /def:%src%\libvvp.def /machine:X64
+dir
+CL.EXE /O2 /LD /EHsc /Feshim.vpi /I. %src%\vpi.c libvvp.lib ivlng.lib /link /DLL /EXPORT:vlog_startup_routines
+dir
